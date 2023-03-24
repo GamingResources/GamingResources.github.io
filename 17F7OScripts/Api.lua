62a72907769b62ad758e99a26c86c2bb0490c4e1
GamingResources <117304964+GamingResources@users.noreply.github.com>
2023-03-24 09:52:02 +0300

    Update Api.lua

--- 17F7OScripts/Api.lua
+++ 17F7OScripts/Api.lua
@@ -1,94 +1,39 @@
-local api = {
-	["GetPlace"] = function()
-		return game.PlaceId
-	end,
-	["GetJobId"] = function()
-		return game.JobId
-	end,
-	["GetHWID"] = function()
-		return game:GetService("RbxAnalyticsService"):GetClientId()
-	end,
-	["GetUserName"] = function()
-		return game.Players.LocalPlayer.Name
-	end,
-	["GetUserId"] = function()
-		return game.Players.LocalPlayer.UserId
-	end,
-	["ReadFile"] = function(foldername, filename)
-		if readfile and writefile then
-			if isfile(foldername .. "/" .. filename) then
-				return readfile(foldername .. "/" .. filename)
-			end
-		end
-	end,
-	["WriteFile"] = function(foldername, filename, text)
-		if readfile and writefile then
-			if isfile(foldername .. "/" .. filename) then
-				writefile(foldername .. "/" .. filename , text)
-			else 
-				makefolder(foldername) 
-				writefile(foldername .. "/" .. filename , text)
-			end
-		end
-	end,
-	["IsExist"] = function(obj)
-		if obj ~= nil then
-			return true
-		end
-	end,
-	["IsSynapse"] = function()
-		if syn then
-			return true
-		else
-			return false
-		end
-	end,
-	['IsKrnl'] = function()
-		if Krnl then
-			return true
-		else
-			return false
-		end
-	end,
-	["GetBlacklist"] = function()
-		if syn then
-			if game.Players.LocalPlayer.UserId == 1 then
-				game:Shutdown()
-			elseif game.Players.LocalPlayer.UserId == 2 then 
-				game:Shutdown()
-			elseif game.Players.LocalPlayer.UserId == 3 then 
-				game:Shutdown()
-			elseif game.Players.LocalPlayer.UserId == 4 then 
-				game:Shutdown()
-			end
-		end
-	end,
-	['GetExploit'] = function()
-		local exploit =
-        (syn and not is_sirhurt_closure and not pebc_execute and "Synapse X") or
-        (SENTINEL_LOADED and "Sentinel") or
-        (is_sirhurt_closure and "Sirhurt") or
-        (PROTOSMASHER_LOADED and "ProtoSmasher") or
-        (KRNL_LOADED and "Krnl") or
-        (WRDAPI and "WeAreDevs") or
-        (isvm and "Proxo") or
-        (shadow_env and "Shadow") or
-        (jit and "EasyExploits") or
-        (getreg()['CalamariLuaEnv'] and "Calamari") or
-        (unit and "Unit") or
-        (IS_VIVA_LOADED and "VIVA") or
-        (IS_COCO_LOADED and "Coco") or
-        (IsElectron and "Electron") or
-        (getexecutorname and import and "Scriptware") or
-        "No Exploit, Wtf?"
-		return exploit
-	end,
-	['ScriptLoadedT'] = function()
-		    print("True")
-        ['ScriptLoadedF'] = function()
-			print("False")
+Readfile(function(foldername, filename)
+	if readfile and writefile then
+		if isfile(foldername .. "/" .. filename) then
+			return readfile(foldername .. "/" .. filename)
 		end
 	end
-}
+end)
 
-return api
+Writefile(function(foldername, filename, text)
+	if readfile and writefile then
+		if isfile(foldername .. "/" .. filename) then
+			writefile(foldername .. "/" .. filename , text)
+		else 
+			makefolder(foldername) 
+			writefile(foldername .. "/" .. filename , text)
+		end
+	end
+end)
+
+Getexploit(function()
+	local exploit =
+	(syn and not is_sirhurt_closure and not pebc_execute and "Synapse X") or
+	(SENTINEL_LOADED and "Sentinel") or
+	(is_sirhurt_closure and "Sirhurt") or
+	(PROTOSMASHER_LOADED and "ProtoSmasher") or
+	(KRNL_LOADED and "Krnl") or
+	(WRDAPI and "WeAreDevs") or
+	(isvm and "Proxo") or
+	(shadow_env and "Shadow") or
+	(jit and "EasyExploits") or
+	(getreg()['CalamariLuaEnv'] and "Calamari") or
+	(unit and "Unit") or
+	(IS_VIVA_LOADED and "VIVA") or
+	(IS_COCO_LOADED and "Coco") or
+	(IsElectron and "Electron") or
+	(getexecutorname and import and "Scriptware") or
+	"No Exploit, Wtf?"
+	return exploit
+end)
